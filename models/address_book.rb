a8965e8992f97cb6f6a254248c5e0b6bb7277d03
joelbeger <jbeger@digitalposition.com>
2021-02-18 09:43:55 -0500

    module 7

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,7 +1,7 @@
 require_relative 'entry'
 require "csv"
 
-    class AddressBook
+ class AddressBook
       attr_reader :entries
 
       def initialize
@@ -32,6 +32,27 @@ require "csv"
         row_hash = row.to_hash
         add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
       end
+  end
+  def binary_search(name)
+    lower = 0
+     upper = entries.length - 1
+ 
+     # #2
+     while lower <= upper
+       # #3
+       mid = (lower + upper) / 2
+       mid_name = entries[mid].name
+ 
+       # #4
+       if name == mid_name
+         return entries[mid]
+       elsif name < mid_name
+         upper = mid - 1
+       elsif name > mid_name
+         lower = mid + 1
+       end
+     end
+     return nil
   end
 
 end
